a586b3480c263f41fa4c165f66564a95525487b5
Eric Luchetti <eric.luchetti@hotmail.com>
2023-10-29 22:54:35 -0300

    Entrega03(gui): Login funcionando

--- ProjetoInterface/LoginController.java
+++ ProjetoInterface/LoginController.java
@@ -14,11 +14,24 @@ public class LoginController {
                 if (username.equals("admin") && password.equals("admin123")) {
                     // Usuário é admin, redirecione para a tela de sucesso
                     JOptionPane.showMessageDialog(null, "Login com sucesso (Admin)!");
-                    // Aqui você pode redirecionar para a tela de sucesso do admin
+                    // Aqui você pode abrir a tela de "Lista de livros"
+                    openListaDeLivrosPage();
                 } else {
                     // Usuário não é admin, exibe mensagem de erro
                     JOptionPane.showMessageDialog(null, "Erro ao fazer login!");
                 }
+            }
+
+            // Método para abrir a página "Lista de livros"
+            private void openListaDeLivrosPage() {
+                JFrame listaDeLivrosFrame = new JFrame("Lista de Livros");
+                // Configurar a janela "Lista de livros" aqui
+                listaDeLivrosFrame.setSize(800, 600);
+                listaDeLivrosFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                // Adicione os componentes da lista de livros aqui
+
+                // Exiba a janela
+                listaDeLivrosFrame.setVisible(true);
             }
         };
     }
