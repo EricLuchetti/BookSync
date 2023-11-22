1e6127a6626fc9b23945a582a14df2f53301feab
yMuller01 <henriquemullercs@gmail.com>
2023-11-22 20:56:56 -0300

    Login com BD e Direcionamento de botões
    Alteração do login com o Banco de dados e alteração do direcionamento dos botões do menu

---
+++ src/backEnd/DatabaseConnector.java
@@ -0,0 +1,24 @@
+package backEnd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnector {
+
+    //Dentro dessa classe existem as credenciais do DB e a função de Conexão com o mesmo
+
+    private static final String URL = "jdbc:mysql://localhost:3306/booksync";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection connect() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to connect to the database.");
+        }
+    }
+}
---
+++ src/backEnd/LoginHandler.java
@@ -0,0 +1,47 @@
+package backEnd;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class LoginHandler {
+
+    //Dentro dessa Classe temos o script de login
+
+    public static Void login(String username, String password) {
+        String encryptedPassword = Services.encryptPassword(password);
+
+        try (Connection connection = DatabaseConnector.connect()) {
+            String query = "SELECT * FROM users WHERE login = ? AND password = ?";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+                preparedStatement.setString(1, username);
+                preparedStatement.setString(2, encryptedPassword);
+
+                try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                    if (resultSet.next()) {
+                        System.out.println(resultSet.getString("admin"));
+                        if (Integer.parseInt(resultSet.getString("admin"))==0) {
+                            frontEnd.ProjetoInterface.TelaUsuario.main(null);
+                        }
+                        else{
+                            frontEnd.ProjetoInterface.TelaAdm.main(null);
+                        }
+                    } else {
+                        //TODO tela de erro de login
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    return null;
+}
+
+    
+//DEBUG
+    public static void main(String[] args) {
+        //String result = login("john_doe", "password123");
+       //System.out.println(result);
+    }
+}
