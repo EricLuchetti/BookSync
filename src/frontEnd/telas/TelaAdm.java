95f34b88b2b56d2ba0287ae6ec4aa3cdb6405901
yMuller01 <henriquemullercs@gmail.com>
2023-11-22 22:50:45 -0300

    Telas de Menu
    Alterado Telas de Menu, tanto de ADM quanto de Usuário com o Action Listener para as duas(falta alterar o caminho correto) e Layout das telas

--- src/frontEnd/telas/TelaAdm.java
+++ src/frontEnd/telas/TelaAdm.java
@@ -2,6 +2,8 @@ package frontEnd.telas;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.geom.RoundRectangle2D;
 
 class RoundButton extends JButton {
@@ -53,37 +55,22 @@ public class TelaAdm {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // Define o tamanho da janela
-        frame.setSize(1300, 650);
+        frame.setSize(1500, 650);
 
         // Coloca a janela no centro da tela
         frame.setLocationRelativeTo(null);
-
-        try {
-            // Tenta carregar e definir um ícone para a janela
-            ImageIcon icon = new ImageIcon("src\\frontEnd\\imgs\\Icon.PNG");
-            frame.setIconImage(icon.getImage());
-        } catch (Exception e) {
-            // Se houver um erro ao carregar o ícone, imprime o erro
-            e.printStackTrace();
-        }
 
         // Cria um painel personalizado com um fundo colorido degradê
         JPanel panel = new JPanel() {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
+                 // Carrega a imagem de fundo
+                ImageIcon backgroundImage = new ImageIcon("src/frontEnd/imgs/FundoBookSync.jpg");
+                Image img = backgroundImage.getImage();
 
-                // Define as cores de início e fim para o degradê
-                Color startColor = new Color(255, 165, 0);
-                Color endColor = new Color(128, 0, 128);
-
-                // Cria um objeto de degradê
-                GradientPaint gradient = new GradientPaint(0, 0, startColor, getWidth(), getHeight(), endColor);
-
-                // Configura o objeto Graphics2D para usar o degradê e preenche o painel com as cores
-                Graphics2D g2d = (Graphics2D) g;
-                g2d.setPaint(gradient);
-                g2d.fillRect(0, 0, getWidth(), getHeight());
+                // Desenha a imagem de fundo no painel
+                g.drawImage(img, 0, 0, getWidth(), getHeight(), this);
             }
         };
 
@@ -107,13 +94,40 @@ public class TelaAdm {
         buttonPanel.setOpaque(false);
 
         // Cria três botões estilizados e os adiciona ao painel de botões
-        RoundButton newsButton = createStyledButton("Gerenciar livros", 300, 400);
-        RoundButton bookShowcaseButton = createStyledButton("Vitrine de Livros", 300, 400);
-        RoundButton addBookButton = createStyledButton("Adicionar Livro", 300, 400);
+        RoundButton newsButton = createStyledButton("Gerenciar livros", 300, 400, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frontEnd.telas.BookSync.main(null);
+                frame.dispose();
+            }
+        });
+
+        RoundButton bookShowcaseButton = createStyledButton("Vitrine de Livros", 300, 400, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+        });
+
+        RoundButton addBookButton = createStyledButton("Adicionar Livro", 300, 400, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        RoundButton registerButton = createStyledButton("Cadastro Usuário", 300, 400, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                frame.dispose();
+            }
+        });
 
         buttonPanel.add(newsButton);
         buttonPanel.add(bookShowcaseButton);
         buttonPanel.add(addBookButton);
+        buttonPanel.add(registerButton);
 
         // Adiciona o painel de botões ao painel principal na região Centro
         panel.add(buttonPanel, BorderLayout.CENTER);
@@ -124,12 +138,13 @@ public class TelaAdm {
     }
 
     // Este método cria e configura um botão com um estilo específico
-    private static RoundButton createStyledButton(String text, int width, int height) {
+    private static RoundButton createStyledButton(String text, int width, int height, ActionListener actionListener) {
         RoundButton button = new RoundButton(text);
         button.setFont(new Font("Arial", Font.BOLD, 25));
         button.setPreferredSize(new Dimension(width, height));
         button.setBackground(new Color(195, 145, 128));
         button.setForeground(Color.WHITE);
+        button.addActionListener(actionListener);
         return button;
     }
 }
